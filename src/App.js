d24965edf0bb990e2725d0a4c597fe73278dbcd0
t-emit <t.siddiqui2814@gmail.com>
2026-04-08 12:06:55 +0530

    fix: removed unused imports and errant requirements file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
